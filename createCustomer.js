11012623b9424868e9a7d5cebe413cbba4c8c059
Duong Vi Phat <viphat@gmail.com>
2020-01-15 13:06:16 +0700

    Check Illogical Data

--- createCustomer.js
+++ createCustomer.js
@@ -2,7 +2,52 @@ import { db } from './database';
 import { provinces } from './provinces';
 import _ from 'lodash'
 
-export const checkMissingData = (customer) => {
+const checkIllogicalData = (customer) => {
+  customer.illogicalData = 0
+  customer.illogicalPhone = 0
+  customer.illogicalAge = 0
+  customer.illogicalAgePupil = 0
+  customer.illogicalAgeStudent = 0
+  customer.illogicalAgeOthers = 0
+
+  var phone = customer.phoneNumber || customer.parentPhoneNumber
+
+  if (!_.isEmpty(phone)) {
+    phone = '' + phone.replace(/[\.\-\_\s\+\(\)]/g,'');
+    if (!_.isNumber(phone)) {
+      customer.illogicalPhone = 1;
+      customer.illogicalData = 1;
+    } else {
+      if (phone.length < 8 || phone.length > 12) {
+        customer.illogicalPhone = 1;
+        customer.illogicalData = 1;
+      }
+    }
+  }
+
+  if (!_.isEmpty(customer.age)) {
+    if (!_.isNumber(customer.age)) {
+      customer.illogicalAge = 1
+      customer.illogicalData = 1
+    } else {
+      var age = parseInt(customer.age)
+      if (customer.groupId === 1 && age < 10 && age >= 18) {
+        customer.illogicalAgePupil = 1
+        customer.illogicalData = 1
+      } else if (customer.groupId === 2 && age < 18 && age >= 30)  {
+        customer.illogicalAgeStudent = 1
+        customer.illogicalData = 1
+      } else (customer.groupId === 3 && age < 18) {
+        customer.illogicalAgeOthers = 1
+        customer.illogicalData = 1
+      }
+    }
+  }
+
+  return customer;
+}
+
+const checkMissingData = (customer) => {
   customer.missingData = 0
   customer.missingLivingCity = 0
   customer.missingName = 0
@@ -27,7 +72,7 @@ export const checkMissingData = (customer) => {
     customer.missingData = 1
   }
 
-  if (customer.missingContactInformation === 0 && cusomer.groupId === 3 && _.isEmpty(customer.phoneNumber) && _.isEmpty(customer.facebook) && _.isEmpty(customer.email)) {
+  if (customer.missingContactInformation === 0 && customer.groupId === 3 && _.isEmpty(customer.phoneNumber) && _.isEmpty(customer.facebook) && _.isEmpty(customer.email)) {
     customer.missingContactInformation = 1
     customer.missingData = 1
   }
@@ -89,11 +134,12 @@ export const createCustomer = (customer) => {
     }
 
     customer = checkMissingData(customer)
+    customer = checkIllogicalData(customer)
 
     db.run('INSERT INTO customers(\
-          name, provinceId, schoolName, age, phoneNumber, parentPhoneNumber, facebook, email, contactInformation, kotexData, dianaData, laurierData, whisperData, othersData, createdAt, notes, receivedGift, groupId, batch, missingData, missingLivingCity, missingName, missingContactInformation, missingAge, missingSchoolName, missingBrandUsing, missingGroup\
+          name, provinceId, schoolName, age, phoneNumber, parentPhoneNumber, facebook, email, contactInformation, kotexData, dianaData, laurierData, whisperData, othersData, createdAt, notes, receivedGift, groupId, batch, hasError, missingData, missingLivingCity, missingName, missingContactInformation, missingAge, missingSchoolName, missingBrandUsing, missingGroup, illogicalData, illogicalPhone, illogicalAge, illogicalAgePupil, illogicalAgeStudent, illogicalAgeOthers\
         ) \
-        VALUES($name, $provinceId, $schoolName, $age, $phoneNumber, $parentPhoneNumber, $facebook, $email, $contactInformation, $kotexData, $dianaData, $laurierData, $whisperData, $othersData, $createdAt, $notes, $receivedGift, $groupId, $batch, $missingData, $missingLivingCity, $missingName, $missingContactInformation, $missingAge, $missingSchoolName, $missingBrandUsing, $missingGroup);',
+        VALUES($name, $provinceId, $schoolName, $age, $phoneNumber, $parentPhoneNumber, $facebook, $email, $contactInformation, $kotexData, $dianaData, $laurierData, $whisperData, $othersData, $createdAt, $notes, $receivedGift, $groupId, $batch, $hasError, $missingData, $missingLivingCity, $missingName, $missingContactInformation, $missingAge, $missingSchoolName, $missingBrandUsing, $missingGroup, $illogicalData, $illogicalPhone, $illogicalAge, $illogicalAgePupil, $illogicalAgeStudent, $illogicalAgeOthers);',
     {
       $name: customer.name,
       $provinceId: customer.provinceId,
@@ -114,6 +160,7 @@ export const createCustomer = (customer) => {
       $receivedGift: customer.receivedGift,
       $groupId: customer.groupId,
       $batch: customer.batch,
+      $hasError: customer.hasError,
       $missingData: customer.missingData,
       $missingLivingCity: customer.missingLivingCity,
       $missingName: customer.missingName,
@@ -121,7 +168,13 @@ export const createCustomer = (customer) => {
       $missingAge: customer.missingAge,
       $missingSchoolName: customer.missingSchoolName,
       $missingBrandUsing: customer.missingBrandUsing,
-      $missingGroup: customer.missingGroup
+      $missingGroup: customer.missingGroup,
+      $illogicalData: customer.illogicalData,
+      $illogicalPhone: customer.illogicalPhone,
+      $illogicalAge: customer.illogicalAge,
+      $illogicalAgePupil: customer.illogicalAgePupil,
+      $illogicalAgeStudent: customer.illogicalAgeStudent,
+      $illogicalAgeOthers: customer.illogicalAgeOthers
     }, (errRes) => {
       db.get('SELECT last_insert_rowid() as customerId', (err, row) => {
         customer.customerId = row.customerId;
