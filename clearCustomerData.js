145a46d1fd12ab2a7259ea50cfef7560416b02ad
Duong Vi Phat <viphat@gmail.com>
2020-10-10 21:05:39 +0900

    FEM 2020 - Update Database

--- clearCustomerData.js
+++ clearCustomerData.js
@@ -1,7 +1,7 @@
 import { db } from './database.js';
 
-export const clearCustomerData = (batch) => {
+export const clearCustomerData = (batch, source) => {
   return new Promise((resolve, reject) => {
-    resolve(db.run('DELETE FROM customers WHERE customers.batch = ?', batch));
+    resolve(db.run('DELETE FROM customers WHERE customers.batch = ? AND customers.source = ?', batch, source));
   });
 }
